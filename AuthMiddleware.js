0253caddc1030144f1ad61194808fe02457ff8c5
Userfor966 <creativeproagency09@gmail.com>
2025-03-12 02:15:08 +0400

    All

--- AuthMiddleware.js
+++ AuthMiddleware.js
@@ -1,7 +1,5 @@
 const jwt = require("jsonwebtoken");
 const authMiddleware = (req, res, next) => {
-    console.log("Gelen Çerezler:", req.cookies); // Çerezleri kontrol et
-    console.log("Gelen Token:", req.cookies.token); // Token doğru geliyor mu?
   
     const token = req.cookies.token;
     if (!token) {
@@ -11,10 +9,8 @@ const authMiddleware = (req, res, next) => {
     try {
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
       req.user = { id: decoded.id }; // Hata: req.user.id yerine objeyi doğrudan oluştur!
-      console.log("Doğrulanan Kullanıcı ID:", decoded.id);
       next();
     } catch (error) {
-      console.error("JWT Doğrulama Hatası:", error.message);
       return res.status(401).json({ message: "Geçersiz token!" });
     }
   };
--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(cors({
   origin: [
     'http://localhost:3000',
-    "https://jobly-roan.vercel.app/"
+    "https://jobly-roan.vercel.app"
   ], 
   methods: ['GET', 'POST','PATCH','PUT','DELETE'],
   credentials: true,  
