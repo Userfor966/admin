c46ebddb6401aea41a005a2fe34559f58b2f2604
Userfor966 <creativeproagency09@gmail.com>
2025-03-13 01:50:02 +0400

    Finish2

--- users/routes/UserRoute.js
+++ users/routes/UserRoute.js
@@ -193,7 +193,7 @@ router.post("/login", async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: true,
-      sameSite: "lax",
+      sameSite: "None",
       path: "/",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
