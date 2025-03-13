c35f6ab38d5051a418967951a54bb9b9ab8975a9
Userfor966 <creativeproagency09@gmail.com>
2025-03-13 18:36:03 +0400

    refresh2

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(cors({
-  origin:'https://jobly-roan.vercel.app/',
+  origin:'https://jobly-roan.vercel.app',
   methods: ['GET', 'POST','PATCH','PUT','DELETE'],
   credentials: true,  
 }));
